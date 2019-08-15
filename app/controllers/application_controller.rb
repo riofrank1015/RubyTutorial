31ae9148e6d12b8c06a863628d42eb2d585c24ba
Ryo <eintracht.frankbb18@gmail.com>
2019-08-15 21:01:45 +0900

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+<<<<<<< HEAD
+=======
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
+>>>>>>> route&controller
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,11 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
+<<<<<<< HEAD
   # config.active_job.queue_name_prefix = "beginner_#{Rails.env}"
+=======
+  # config.active_job.queue_name_prefix = "project_#{Rails.env}"
+>>>>>>> route&controller
 
   config.action_mailer.perform_caching = false
 
